47e3596764686981ea2730ce14431cbb6f5372da
unknown <quentin.heraud@epitech.eu>
2020-09-11 15:03:49 +0200

    Fix : package.json updated

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 require('rootpath')();
+
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const jwt = require('_helpers/jwt');
 const errorHandler = require('_helpers/error-handler');
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
